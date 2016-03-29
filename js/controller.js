480ae4710358ef53e84374d83af6efb97be12430
iny7 <466924784@qq.com>
2016-03-29 13:16:28 +0800

    增加了男孩转身的功能
    在map上增加了简单的事件绑定,让男孩通过事件监听来控制行动,另外利用css3animation的direction属性(取reverse)来控
    制动画反向播放从而转身

---
+++ js/controller.js
@@ -0,0 +1,155 @@
+function Controller(arr, options){
+	
+	var map = new Map(arr, options);
+
+	var boy = new Boy($('#boy'));
+
+	//当一个动画正在进行的时候,不允许其他动画进行
+	var flag = true;
+
+	this.init = function(){
+		this.initEvent();
+		this.generateTinyMap();
+	}
+
+	//生成小地图
+	this.generateTinyMap = function(){
+		var maxLength = 0;
+		for(var i = 0 ; i < arr.length ; i ++){
+			maxLength = arr[i].length > maxLength ? arr[i].length : maxLength;
+		}	
+	}
+
+	$(map).on('transitionend', function(event) {
+		flag = true;
+		boy.stop();
+	});
+
+	//初始化事件
+	this.initEvent = function(){
+		for(var i = 0 ; i < arr.length ; i ++){
+			for(var j = 0 ; j < arr[i].length ; j ++){
+				//首先筛选出不是墙的元素
+				if(arr[i][j] === undefined){
+					continue;
+				}
+				//对于非墙元素,上面有的,让他可以向上
+				if(arr[i-1] && arr[i-1][j] != undefined){
+					// console.log(i+"-"+j+":"+arr[i][j]+"上")
+					this.moveTop(i, j)
+				}
+				//下面有
+				if(arr[i+1] && arr[i+1][j] != undefined){
+					// console.log(i+"-"+j+":"+arr[i][j]+"下")
+					this.moveBottom(i, j)
+				}
+				//左边有
+				if(arr[j-1] && arr[i][j-1] != undefined){
+					// console.log(i+"-"+j+":"+arr[i][j]+"左")
+					this.moveLeft(i, j)
+				}
+				//右边有
+				if(arr[j+1] && arr[i][j+1] != undefined){
+					// console.log(i+"-"+j+":"+arr[i][j]+"右")
+					this.moveRight(i, j)
+				}
+			}
+		}
+	}
+
+	this.moveTop = function(i, j){
+		arr[i][j].on('mousewheel', function(event) {
+			var x = event.originalEvent.deltaX;
+			var y = event.originalEvent.deltaY;
+			if(flag && Math.abs(y) > Math.abs(x) && y < -10){
+
+				boy.walk()
+				map.moveTop(i, j, options.animationTime)
+				flag = false;
+				
+				// console.log("top")
+			}
+		})
+	}
+	this.moveBottom = function(i, j){
+		arr[i][j].on('mousewheel', function(event) {
+			var x = event.originalEvent.deltaX;
+			var y = event.originalEvent.deltaY;
+			if(flag && Math.abs(x) < Math.abs(y) && y > 10){
+
+				boy.walk()
+				map.moveBottom(i, j, options.animationTime)
+
+				flag = false;
+
+				// console.log("bottom")
+			}
+		})
+	}
+
+	this.moveLeft = function(i, j){
+		arr[i][j].on('mousewheel', function(event) {
+			var x = event.originalEvent.deltaX;
+			var y = event.originalEvent.deltaY;
+			if(flag && Math.abs(x) > Math.abs(y) && x < -10){
+
+				//如果男孩朝向与目标方向相同
+				if(boy.direction === 'left'){
+					//直接移动
+					boy.walk()
+					map.moveLeft(i, j, options.animationTime)
+				}
+				//否则
+				else{
+					//男孩先转向,再移动,页面wait一个delayTime
+					boy.turnAndWalk()
+					map.moveLeft(i, j, options.animationTime, options.animationDelay)
+				}
+
+				flag = false;
+
+				// console.log("left")
+			}
+		})
+	}
+	this.moveRight = function(i, j){
+		arr[i][j].on('mousewheel', function(event) {
+			var x = event.originalEvent.deltaX;
+			var y = event.originalEvent.deltaY;
+			if(flag && Math.abs(x) > Math.abs(y) && x > 10){
+
+				//如果男孩朝向与目标方向相同
+				if(boy.direction === 'right'){
+					//直接移动
+					boy.walk()
+					map.moveRight(i, j, options.animationTime)
+				}
+				//否则
+				else{
+					//男孩先转向,再移动,页面wait一个delayTime
+					boy.turnAndWalk()
+					map.moveRight(i, j, options.animationTime, options.animationDelay)
+				}
+
+				flag = false;
+
+				// console.log("right")
+			}
+		})
+	}
+
+	this.init()
+
+	map.addEvent('vertical',function(){
+		boy.walk();
+	});
+
+	//水平移动
+	map.addEvent('horizontal',function(){
+		boy.walk();
+	});
+	//垂直移动
+	map.addEvent('stop',function(){
+		boy.stop();
+	});
+}
--- js/script.js
+++ js/script.js
@@ -7,118 +7,68 @@
 
 function Map(arr, options){
 
+	var _this = this;
+
 	var box = $("[data-map='box']");
 	var border = $("[data-map='border']")
-	box.css('transition', 'all 1s');
-	border.css('transition', 'all 1s');
+	// console.log('all '+ options.animationTime +'s '+options.animationDelay+ 's');
 	
-	this.init = function(){
-		this.initEvent();
-		this.generateTinyMap();
+	//当box触发动画结束事件时,将事件传递给$(map),从而被Controller接收到
+	box.on('transitionend', function(event) {
+		$(_this).trigger('transitionend')
+	});
+
+	/*简单的单事件模型*/
+	this.Events = []
+
+	this.addEvent = function(name, func){
+		if(!this.Events[name]){
+			this.Events[name] = new Array;
+		}
+		this.Events[name].push(func)
 	}
 
-	//初始化事件
-	this.initEvent = function(){
-		for(var i = 0 ; i < arr.length ; i ++){
-			for(var j = 0 ; j < arr[i].length ; j ++){
-				//首先筛选出不是墙的元素
-				if(arr[i][j] === undefined){
-					continue;
-				}
-				//对于非墙元素,上面有的,让他可以向上
-				if(arr[i-1] && arr[i-1][j] != undefined){
-					console.log(i+"-"+j+":"+arr[i][j]+"上")
-					this.moveTop(i, j)
-				}
-				//下面有
-				if(arr[i+1] && arr[i+1][j] != undefined){
-					console.log(i+"-"+j+":"+arr[i][j]+"下")
-					this.moveBottom(i, j)
-				}
-				//左边有
-				if(arr[j-1] && arr[i][j-1] != undefined){
-					console.log(i+"-"+j+":"+arr[i][j]+"左")
-					this.moveLeft(i, j)
-				}
-				//右边有
-				if(arr[j+1] && arr[i][j+1] != undefined){
-					console.log(i+"-"+j+":"+arr[i][j]+"右")
-					this.moveRight(i, j)
-				}
-			}
+	//简单实现单事件绑定,不实用栈或者队列
+	this.fireEvent = function(name){
+		var funs = this.Events[name];
+		funs[0]();
+	}
+	this.removeEvent = function(name){
+		//清空事件队列
+		this.Events[name] = null;
+	}
+	this.initTime = function(time, delay){
+		if(delay){
+			box.css('transition', 'all '+ time +'s '+delay+ 's');
+			border.css('transition', 'all '+ time +'s '+delay+ 's');
+		}
+		else{
+			box.css('transition', 'all '+ time +'s');
+			border.css('transition', 'all '+ time +'s');
 		}
 	}
 
-	//生成小地图
-	this.generateTinyMap = function(){
-		var maxLength = 0;
-		for(var i = 0 ; i < arr.length ; i ++){
-			maxLength = arr[i].length > maxLength ? arr[i].length : maxLength;
-		}	
-		console.log(maxLength)
+	this.moveTop = function(i, j, time){
+		this.initTime(time);
+		box.css('transform', 'translateX(-'+j+'00%) translateY(-'+(i-1)+'00%)')
+		border.css('transform', 'translateX('+j+'00%) translateY('+(i-1)+'00%)')
 	}
-	
-
-	//当一个动画正在进行的时候,不允许其他动画进行
-	var flag = true;
-
-	var canMove = function(){
-		return flag;
+	this.moveBottom = function(i, j, time){
+		this.initTime(time);
+		box.css('transform', 'translateX(-'+j+'00%) translateY(-'+(i+1)+'00%)')
+		border.css('transform', 'translateX('+j+'00%) translateY('+(i+1)+'00%)')
 	}
-	box.on('transitionend', function(event) {
-		flag = true;
-	});
-
-	this.moveTop = function(i, j){
-		arr[i][j].on('mousewheel', function(event) {
-			var x = event.originalEvent.deltaX;
-			var y = event.originalEvent.deltaY;
-			if(flag && Math.abs(y) > Math.abs(x) && y < -10){
-				flag = false;
-				box.css('transform', 'translateX(-'+j+'00%) translateY(-'+(i-1)+'00%)')
-				border.css('transform', 'translateX('+j+'00%) translateY('+(i-1)+'00%)')
-				console.log("top")
-			}
-		})
+	this.moveLeft = function(i, j, time, delay){
+		this.initTime(time, delay);
+		box.css('transform', 'translateX(-'+(j-1)+'00%) translateY(-'+i+'00%)')
+		border.css('transform', 'translateX('+(j-1)+'00%) translateY('+i+'00%)')	
 	}
-	this.moveBottom = function(i, j){
-		arr[i][j].on('mousewheel', function(event) {
-			var x = event.originalEvent.deltaX;
-			var y = event.originalEvent.deltaY;
-			if(flag && Math.abs(x) < Math.abs(y) && y > 10){
-				flag = false;
-				box.css('transform', 'translateX(-'+j+'00%) translateY(-'+(i+1)+'00%)')
-				border.css('transform', 'translateX('+j+'00%) translateY('+(i+1)+'00%)')
-				console.log("bottom")
-			}
-		})
-	}
-	this.moveLeft = function(i, j){
-		arr[i][j].on('mousewheel', function(event) {
-			var x = event.originalEvent.deltaX;
-			var y = event.originalEvent.deltaY;
-			if(flag && Math.abs(x) > Math.abs(y) && x < -10){
-				flag = false;
-				box.css('transform', 'translateX(-'+(j-1)+'00%) translateY(-'+i+'00%)')
-				border.css('transform', 'translateX('+(j-1)+'00%) translateY('+i+'00%)')
-				console.log("left")
-			}
-		})
-	}
-	this.moveRight = function(i, j){
-		arr[i][j].on('mousewheel', function(event) {
-			var x = event.originalEvent.deltaX;
-			var y = event.originalEvent.deltaY;
-			if(flag && Math.abs(x) > Math.abs(y) && x > 10){
-				flag = false;
-				box.css('transform', 'translateX(-'+(j+1)+'00%) translateY(-'+i+'00%)')
-				border.css('transform', 'translateX('+(j+1)+'00%) translateY('+i+'00%)')
-				console.log("right")
-			}
-		})
+	this.moveRight = function(i, j, time, delay){
+		this.initTime(time, delay);
+		box.css('transform', 'translateX(-'+(j+1)+'00%) translateY(-'+i+'00%)')
+		border.css('transform', 'translateX('+(j+1)+'00%) translateY('+i+'00%)')			
 	}
 
-	this.init()
 }
 
 $(function(){
@@ -128,11 +78,13 @@ $(function(){
 		[$("#fourth")	, $("#fifth")],
 		[,$("#eighth")	, $("#ninth")]
 	]
-	//应该传个参数,控制比如要不要小地图这种功能
-	var map = new Map(mapArr);
-	//右上角map动态生成?
-	//小男孩是观察者,Map是事件源,这次一定要实现观察者模式自定义事件
-	var boy = $("#boy")
-	console.log(boy)
-	boy.addClass('walk')
+
+	var options = {
+		'animationTime' : '3',
+		'animationDelay' : '1'
+	}
+
+	var ctrl = new Controller(mapArr, options);
+
+
 })
